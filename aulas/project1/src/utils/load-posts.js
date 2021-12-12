9bed6d1966566b759a983f11dc3867bb6baee0b6
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-12 15:06:36 -0300

    feat: create a service to external api.

--- aulas/project1/src/utils/load-posts.js
+++ aulas/project1/src/utils/load-posts.js
@@ -1,13 +1,13 @@
 export const loadPosts = async () => {
-    const postsResponse = fetch('http://jsonplaceholder.typicode.com/posts')
-    const photosResponse = fetch('http://jsonplaceholder.typicode.com/photos')
+  const postsResponse = fetch('http://jsonplaceholder.typicode.com/posts');
+  const photosResponse = fetch('http://jsonplaceholder.typicode.com/photos');
 
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse])
+  const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-    const postsJson = await posts.json()
-    const photosJson = await photos.json()
+  const postsJson = await posts.json();
+  const photosJson = await photos.json();
 
-    return postsJson.map((posts, index) => {
-        return { ...posts, cover: photosJson[index].url }
-    })
-}
+  return postsJson.map((posts, index) => {
+    return { ...posts, cover: photosJson[index].url };
+  });
+};
