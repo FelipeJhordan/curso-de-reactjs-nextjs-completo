6516b330835be08d95d282933dcddfb2ec498f40
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-16 12:55:23 -0300

    feat(react-hooks): aplicando o useState

--- aulas/project2/src/App.js
+++ aulas/project2/src/App.js
@@ -1,17 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const { reverse } = this.state;
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse((prevState) => !prevState);
+  };
+
+  const handleIncrement = () => {
+    setCounter((c) => c + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter((c) => (c == 0 ? c : c - 1));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>Contador: {counter}</h1>
+        <div>
+          <button type="button" onClick={handleIncrement}>
+            Aumentar
+          </button>
+          <button type="button" onClick={handleDecrement}>
+            Diminuir
+          </button>
+        </div>
+        <button type="button" onClick={handleClick}>
+          {reverseClass ? 'Voltando' : 'Indo'}
+        </button>
       </header>
     </div>
   );
