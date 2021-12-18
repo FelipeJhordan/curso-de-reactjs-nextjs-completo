0685ff6349627a4f183f543c64454e8155256bca
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-18 13:13:54 -0300

    feat(react-hook): using ref hook

--- aulas/project2/src/App.js
+++ aulas/project2/src/App.js
@@ -1,14 +1,13 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
-  console.log('filho renderizou');
-  console.log(post);
-
+const Post = ({ post, onClick: handleCLick }) => {
   return (
     <div className="post">
-      <h1>{post.title}</h1>
+      <h1 style={{ fontSize: '16px' }} onClick={() => handleCLick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -20,33 +19,47 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  onClick: P.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
-  console.log('Pai renderizou!');
-
+  const input = useRef(null);
+  const contador = useRef(0);
   // Component didMount
 
   useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
+
   return (
     <div className="app">
+      <h1>Renderizou: {contador.current}x</h1>
       <p>
-        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+        <input ref={input} type="search" value={value} onChange={(e) => setValue(e.target.value)} />
       </p>
       {/* /*memorizar component */}
       {useMemo(() => {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post key={post.id} post={post} />;
+            return <Post key={post.id} post={post} onClick={handleClick} />;
           })
         );
       }, [posts])}
