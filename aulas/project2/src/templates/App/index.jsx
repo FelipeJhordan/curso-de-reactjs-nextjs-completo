3f7574851fc8004d4854e29f6e516b50e3497969
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-22 13:40:05 -0300

    feat(react-hook): async hook

--- aulas/project2/src/templates/App/index.jsx
+++ aulas/project2/src/templates/App/index.jsx
@@ -1,98 +1,63 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB);
-};
-
-const useFetch = (url, options) => {
+const useAsync = (asyncFunction, shouldRun = true) => {
   const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [shouldLoad, setShouldLoad] = useState(false);
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+
+  const run = useCallback(async () => {
+    await new Promise((r) => setTimeout(r, 2000));
+
+    setResult(null);
+    setError(null);
+    setStatus('pending');
+
+    return asyncFunction()
+      .then((response) => {
+        setStatus('settled');
+        setResult(response);
+      })
+      .catch((error) => {
+        setStatus('error');
+        setError(error);
+      });
+  }, [asyncFunction]);
 
   useEffect(() => {
-    let changed = false;
-    if (!isObjectEqual(url, urlRef.current)) {
-      urlRef.current = url;
-      changed = true;
+    if (shouldRun) {
+      run();
     }
-    if (!isObjectEqual(options, optionsRef.current)) {
-      optionsRef.current = options;
-      changed = true;
-    }
+  }, [run, shouldRun]);
 
-    if (changed) {
-      setShouldLoad((s) => !s);
-    }
-  }, [url, options]);
-
-  useEffect(() => {
-    let wait = false;
-    const controller = new AbortController();
-    const signal = controller.signal;
-    setLoading(true);
-    const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 1000));
-      try {
-        const response = await fetch(urlRef.current, { signal, ...optionsRef.current });
-        const jsonResult = await response.json();
-        if (!wait) {
-          setResult(jsonResult);
-          setLoading(false);
-        }
-      } catch (e) {
-        setLoading(false);
-        console.warn(e.message);
-      }
-    };
-    fetchData();
-
-    return () => {
-      wait = true;
-      controller.abort();
-    };
-  }, [shouldLoad]);
-
-  return [result, loading];
+  return [run, result, error, status];
 };
 
-const App = () => {
-  const [postId, setPostId] = useState('');
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
-    headers: {
-      abc: 1 + postId,
-    },
-  });
-
-  useEffect(() => {
-    console.log('Id do post ' + postId);
-  }, [postId]);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  const handleClick = (id) => {
-    setPostId(id);
-  };
-  if (!loading && result) {
-    return (
-      <div>
-        {result?.length > 0 ? (
-          result.map((p) => (
-            <p onClick={() => handleClick(p.id)} key={'post-' + p.id}>
-              {p.title}
-            </p>
-          ))
-        ) : (
-          <p onClick={() => handleClick('')}>{result.title}</p>
-        )}
-      </div>
-    );
-  }
-
-  return <h1>Oi</h1>;
+const fetchData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  const json = await data.json();
+  return json;
 };
 
+const Home = () => {
+  const [posts, setPosts] = useState(null);
+  const [reFetchData, result, error, status] = useAsync(fetchData);
+
+  if (status === 'idle') {
+    return <pre>Nada executando</pre>;
+  }
+
+  if (status === 'pending') {
+    return <pre>Loading</pre>;
+  }
+
+  if (status === 'error') {
+    return <pre>Error</pre>;
+  }
+
+  if (status === 'settled') {
+    return <pre>{JSON.stringify(result, null, 2)}</pre>;
+  }
+};
+
+const App = Home;
 export default App;
