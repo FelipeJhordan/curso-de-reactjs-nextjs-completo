8009e3eb7540d86a72b509c7aee7e1c2d2915d8a
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-22 17:10:44 -0300

    feat(react): Using childre/cloneElement

--- aulas/project2/src/templates/App/index.jsx
+++ aulas/project2/src/templates/App/index.jsx
@@ -1,64 +1,23 @@
-import { Component, useEffect, useState } from 'react';
-import P from 'prop-types';
+import React, { cloneElement } from 'react';
+
 const s = {
   style: {
     fontSize: '60px',
   },
 };
 
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    // Atualiza o state para que a próxima renderização mostre a UI alternativa.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // Você também pode registrar o erro em um serviço de relatórios de erro
-    console.log(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // Você pode renderizar qualquer UI alternativa
-      return <h1>Algo deu errado.</h1>;
-    }
-
-    // eslint-disable-next-line react/prop-types
-    return this.props.children;
-  }
-}
-
-const ItWillThrowError = () => {
-  const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    if (counter > 3) {
-      throw new Error('Que chato');
-    }
+const Parent = ({ children }) => {
+  return React.Children.map(children, (child) => {
+    const newChild = cloneElement(child, { ...s });
+    return newChild;
   });
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCounter((s) => s + 1);
-        }}
-      >
-        Click to increase {counter}
-      </button>
-    </div>
-  );
 };
 
 export const Home = () => {
   return (
-    <p {...s}>
-      <ErrorBoundary>
-        <ItWillThrowError />
-      </ErrorBoundary>
-    </p>
+    <Parent>
+      <p>Oi</p>
+      <p>Oi</p>
+    </Parent>
   );
 };
