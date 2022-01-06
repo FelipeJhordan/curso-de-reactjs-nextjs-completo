a800d08331851e74fae1f20270e9f5df4db02d22
Felipe Jhordan <felipejordan.alves@gmail.com>
2022-01-06 12:27:37 -0300

    refactor(next): refactor load-pages to get data by json

--- aulas/project3/src/api/load-pages.js
+++ aulas/project3/src/api/load-pages.js
@@ -1,15 +1,24 @@
 import { urlFindAll, urlFilter } from '../config/index';
 import { mapData } from './map-data';
+import dataMock from './data.json';
+
+const filterDataJsonMock = (pages = [], slugUrl = '') => {
+  return pages.filter(({ slug = '' }) => {
+    return slug.trim() === slugUrl.trim();
+  });
+};
 
 export const loadPages = async (slug = '') => {
   const cleanSlug = slug ? `${slug.replace(/[^]a-z0-9-_/gi, '')}` : '';
   const isFindAll = cleanSlug === '';
-  const url = isFindAll ? urlFindAll : urlFilter(cleanSlug);
-  const raw = await fetch(url);
-  const json = await raw.json();
+  // const url = isFindAll ? urlFindAll : urlFilter(cleanSlug);
+  // const raw = await fetch(url);
+  // const json = await raw.json();
+
+  const json = isFindAll ? dataMock : filterDataJsonMock(dataMock, cleanSlug);
   const data = !isFindAll
-    ? mapData('filled', json.data)[0] || []
-    : mapData('filled', json.data);
+    ? mapData('filled', json)[0] || []
+    : mapData('filled', json);
 
   return data;
 };
